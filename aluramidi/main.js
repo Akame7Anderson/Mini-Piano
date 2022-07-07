b614ad03f4f1ad31642e0a6a846daa51deba1059
Anderson (Akame!) <andersonrobertopetry@gmail.com>
2022-07-07 16:13:33 -0300

    condições

--- aluramidi/main.js
+++ aluramidi/main.js
@@ -5,15 +5,23 @@ function tocaSom(idElementoAudio){
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
-let contador = 0;
 
-while (contador < listaDeTeclas.length){
+//para
+for (let contador = 0; contador < listaDeTeclas.length; contador ++){
     const tecla = listaDeTeclas[contador];
     const instrumento = tecla.classList[1];
     const idAudio = `#som_${instrumento}`;
-    //console.log(idAudio);
     tecla.onclick = function (){
         tocaSom(idAudio);
     }
-    contador = contador + 1;
+
+    tecla.onkeydown = function(evento){
+        if (evento.code == 'Space'){
+            tecla.classList.add('ativa');
+        }
+        
+    }
+    tecla.onkeyup = function (){
+        tecla.classList.remove('ativa');
+    }
 }
