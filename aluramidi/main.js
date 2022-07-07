a78ee9c7ea7272f3247fac51768fab177f73cd19
Anderson (Akame!) <andersonrobertopetry@gmail.com>
2022-07-07 11:32:56 -0300

    otimização com while

--- aluramidi/main.js
+++ aluramidi/main.js
@@ -1,5 +1,14 @@
 function tocaSomPom(){
     document.querySelector('#som_tecla_pom').play();
 }
-document.querySelector(".tecla_pom").onclick = tocaSomPom; //LINKA A TAG OU CLASSE DO HTML NO JS
 
+
+const listaDeTeclas = document.querySelectorAll('.tecla');
+;
+
+let contador = 0;
+
+while (contador < listaDeTeclas.length){
+    listaDeTeclas[contador].onclick = tocaSomPom;
+    contador = contador + 1;
+}
